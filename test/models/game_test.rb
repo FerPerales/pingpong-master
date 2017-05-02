e97806ba0f4af5aecf81ad29262c41bca50d56b7
Fernando Perales <me@ferperales.net>
2017-05-02 14:46:34 -0500

    Add some specs for game model

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  player = User.new(email: 'player@demo.com')
+  opponent = User.new(email: 'opponent@demo.com')
+
+  default_values = {
+    date_played: Date.today,
+    opponent: opponent,
+    player: player,
+    opponent_score: 21,
+    own_score: 17,
+  }
+
+  test "requires opponent score" do
+    game = Game.new(default_values)
+    game.opponent_score = nil
+    assert_equal(game.valid?, false)
+    assert_equal(game.errors[:opponent_score], ["can't be blank"])
+  end
+
+  test "requires own score" do
+    game = Game.new(default_values)
+    game.own_score= nil
+    assert_equal(game.valid?, false)
+    assert_equal(game.errors[:own_score], ["can't be blank"])
+  end
+
+  test "requires a winner" do
+    game = Game.new(default_values)
+    game.own_score= 15
+    game.opponent_score = 12
+    assert_equal(game.valid?, false)
+    assert_equal(game.errors[:base].first, "There must be a winner with at least #{Game::WINNING_SCORE} points")
+  end
 end
